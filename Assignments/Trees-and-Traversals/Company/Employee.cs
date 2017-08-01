0717950bf7c61a4f853e8abcbeb4ecd41399cf57
Dobrin Dimchev <dobrin.yonkov1@gmail.com>
2017-08-01 15:40:29 +0300

    Company task added

---
+++ Assignments/Trees-and-Traversals/Company/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Company
+{
+    class Employee
+    {
+        public List<Employee> Subordings { get; set; }
+        public int Salary { get; set; }
+        public string Name { get; set; }
+
+        public Employee(string name)
+        {
+            this.Name = name;
+            this.Salary = 1;
+            this.Subordings = new List<Employee>();
+        }
+    }
+}
