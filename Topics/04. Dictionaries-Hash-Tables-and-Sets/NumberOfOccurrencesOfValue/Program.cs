a8fdd4f55aac4ec25bc8427292bdcaeda8284767
Dobrin Dimchev <dobrin.yonkov1@gmail.com>
2017-08-04 17:04:43 +0300

    new topic added

---
+++ Topics/04. Dictionaries-Hash-Tables-and-Sets/NumberOfOccurrencesOfValue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfOccurrencesOfValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new double[] { 3, 4, 4, -2.5, 3, 3, 4, 3, -2.5 };
+
+            IDictionary<double, int> numsCount = new SortedDictionary<double, int>();
+            CountNumbers(numbers, numsCount);
+
+        }
+
+        private static void CountNumbers(double[] numbers, IDictionary<double, int> numsCount)
+        {
+            foreach (var number in numbers)
+            {
+                int count = 1;
+                if (numsCount.ContainsKey(number))
+                {
+                    count = numsCount[number] + 1;
+                }
+                numsCount[number] = count;
+            }
+            foreach (var pair in numsCount)
+            {
+                Console.WriteLine("{0} -> {1}", pair.Key, pair.Value);
+            }
+        }
+    }
+}
